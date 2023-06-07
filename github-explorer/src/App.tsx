9addae8acb240dc1685c0797cef440a63693ea58
marcionavarro <marcionavarrodearaujo@gmail.com>
2023-06-07 14:33:58 -0300

    First Commit

---
+++ github-explorer/src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.scss';
+import RepositoryList from './components/RepositoryList';
+
+// https://api.github.com/orgs/rocketseat/repos
+
+function App() {
+  return (
+    <>
+      <RepositoryList />
+    </>
+  )
+}
+
+export default App;
---
+++ github-explorer/src/components/RepositoryItem.tsx
@@ -0,0 +1,19 @@
+interface RepositoryItemProps {
+  repository: {
+    name: string,
+    description: string,
+    html_url: string,
+  }
+}
+
+const RepositoryItem = (props: RepositoryItemProps) => {
+  return (
+    <li>
+      <strong>{props.repository.name}</strong>
+      <p>{props.repository.description}</p>
+      <a href={props.repository.html_url}>Acessar Repositorio</a>
+    </li>
+  );
+}
+
+export default RepositoryItem;
